c5610bf0f2fd23d50730a72a59cac14e133e84be
ldeveloperl1985 <ldeveloperl1985@gmail.com>
2017-08-18 21:38:07 +0530

    change gem

--- server.rb
+++ server.rb
@@ -8,8 +8,23 @@ require 'jwt'
 require 'json'
 require 'sinatra/cross_origin'
 
+set :bind, '0.0.0.0'
+
 configure do
   enable :cross_origin
+end
+
+before do
+  response.headers['Access-Control-Allow-Origin'] = '*'
+end
+
+# routes...
+
+options "*" do
+  response.headers["Allow"] = "GET, POST, OPTIONS"
+  response.headers["Access-Control-Allow-Headers"] = "Authorization, Content-Type, Accept, X-User-Email, X-Auth-Token"
+  response.headers["Access-Control-Allow-Origin"] = "*"
+  200
 end
 
 
